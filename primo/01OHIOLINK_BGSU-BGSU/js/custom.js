c8bd354021c93e71560a1662d52e8f1fc3b4f21c
John Kloor <john@kloor.org>
2025-03-17 14:14:18 -0400

    Update custom javascript and add HTML/SVG for Ask Us! button.

--- primo/01OHIOLINK_BGSU-BGSU/js/custom.js
+++ primo/01OHIOLINK_BGSU-BGSU/js/custom.js
@@ -8,6 +8,11 @@
         templateUrl: url + 'prmTopBarBefore.html'
     });
 
+    // Add BGSU link icons after favorites icon.
+    app.component('prmSearchBookmarkFilterAfter', {
+        templateUrl: url + 'prmSearchBookmarkFilterAfter.html'
+    });
+
     // Show search profile slots by default.
     app.component('prmSearchBarAfter', {
         controller: function($scope) {
@@ -17,7 +22,28 @@
         }
     });
 
-    // Load BGSU University Libraries proactive LibChat.
+    // Collapse the Other Institutions list by default.
+    app.component('prmAlmaOtherMembersAfter', {
+        controller: function($scope) {
+            this.$onInit = function () {
+                $scope.$parent.$ctrl.isCollapsed = true;
+            }
+        }
+    });
+
+    // Automatically search when a profile slot is selected.
+    window.appConfig['primo-view']['attributes-map'].tabsRemote = 'true';
+
+    // Do not open menu links in new tabs/windows.
+    const menu = window.appConfig.tiles.MainMenuTileInterface;
+
+    for (const key in menu) {
+        for (let count = 0; count < menu[key].mainview.length; count++) {
+            menu[key].mainview[count].target = '_self';
+        }
+    }
+
+    // Load BGSU University Libraries proactive chat.
     const script = document.createElement('script');
 
     script.src = 'https://lib.bgsu.edu/template/2.0.0-b20/libchat.js';
